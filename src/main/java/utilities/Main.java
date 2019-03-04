76d0bfba438fe232581e4343c9ca3cc3ad1c9ddb
Martin Bauer <bauer_martin@icloud.com>
2019-03-04 12:35:08 +0100

    Add main method for testing

--- src/main/java/utilities/Main.java
+++ src/main/java/utilities/Main.java
@@ -1,9 +1,13 @@
 package utilities;
 
+import static java.util.stream.Collectors.joining;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.nio.charset.Charset;
+import java.util.stream.Stream;
 
 import commands.CheckSatisfiabilityCommand;
 import commands.LoadVMCommand;
@@ -14,11 +18,26 @@ public final class Main {
   private Main() {
   }
 
-  @SuppressWarnings("UseOfSystemOutOrSystemErr")
   public static void main(String... args) throws IOException {
-    BufferedReader in = new BufferedReader(
+    runShellUsingStandardInput();
+  }
+
+  @SuppressWarnings("unused")
+  private static void runShellUsingStandardInput() throws IOException {
+    BufferedReader input = new BufferedReader(
         new InputStreamReader(System.in, Charset.forName("UTF-8")));
-    Shell shell = new Shell(in, System.out);
+    startShell(input);
+  }
+
+  private static void runShell(String... args) throws IOException {
+    String inputCommands = Stream.of(args).collect(joining(System.lineSeparator()));
+    BufferedReader input = new BufferedReader(new StringReader(inputCommands));
+    startShell(input);
+  }
+
+  @SuppressWarnings("UseOfSystemOutOrSystemErr")
+  private static void startShell(BufferedReader input) throws IOException {
+    Shell shell = new Shell(input, System.out);
     GlobalContext context = new GlobalContext();
     shell.registerCommand(new LoadVMCommand(context), "load-vm");
     shell.registerCommand(new SelectSolverCommand(context), "select-solver");
